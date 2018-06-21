af96adf47ecc322612de8299629934936690d543
梁天峰 <liangtianfeng@xjj.com>
2018-06-21 17:57:57 +0800

    second

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -7,6 +7,22 @@ public class Person {
 
     private String address;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
     public int getId() {
         return id;
     }
