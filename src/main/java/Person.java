cdb6de3e1eacbf6725cfd3ef5274612b04090485
liangtianfeng <13281090@bjtu.edu.cn>
2018-06-21 10:42:45 +0800

    second

---
+++ src/main/java/Person.java
@@ -0,0 +1,21 @@
+public class Person {
+    private int id;
+
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
